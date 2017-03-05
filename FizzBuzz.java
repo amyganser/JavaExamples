7cee189a29f5cb9ee07995436e0212458afbbcad
AmyGanser <amy.christine112@gmail.com>
2017-03-05 10:07:01 -0700

    Simple FizzBuzz Commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,9 +1,11 @@
+//FizzBuzz original
 public class FizzBuzz {
 	public static void main(String args[]){
 		for (int i = 1; i <= 100; i++)
 		{
 			if (i % 15 == 0) 
 			{
+			
 				System.out.println("Fizz Buzz"); 
 			}
 			else if (i % 5 == 0) 
